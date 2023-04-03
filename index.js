3b143ffbeebc35de2aeff739694ae71aa3e4deb4
keshav <tomarkeshavofficial@gmail.com>
2023-04-03 22:00:54 +0530

    cors made online becuase frontend is on vercel now

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://foodie-website-two.vercel.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
