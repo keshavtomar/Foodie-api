923adb23511d2dc97fc6bf9d4f2c7a885ad6942a
keshav <tomarkeshavofficial@gmail.com>
2023-04-03 21:03:13 +0530

    index.js modified acc to employee management

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require('express')
 const app = express()
-const port = 5000
 const mongoDB = require("./db");
+const cors = require('cors');
 mongoDB();
+const v = 5;
 
-
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origiin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
+app.get("/", (req, res) => {
+    res.send({ status: ok });
 })
+
+app.use(cors());
+
+
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(express.json());
 
