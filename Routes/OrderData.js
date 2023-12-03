224fc828ffa229a5558b0a50161601c37d4bb466
keshav <tomarkeshavofficial@gmail.com>
2023-12-03 23:45:05 +0530

    admin added

--- Routes/OrderData.js
+++ Routes/OrderData.js
@@ -2,6 +2,8 @@ const express = require('express');
 const Router = express.Router();
 const Order = require('../models/Orders');
 
+const adminEmail = "admin@gmail.com";
+
 Router.post('/orderData', async (req, res) => {
     let data = req.body.order_data
     await data.splice(0, 0, { Order_date: req.body.order_date })
@@ -9,7 +11,9 @@ Router.post('/orderData', async (req, res) => {
 
     //if email not exisitng in db then create: else: InsertMany()
     let eId = await Order.findOne({ 'email': req.body.email })
+    let adminId = await Order.findOne({'email':adminEmail})
     console.log(eId)
+    let addSuccess = false;
     if (eId === null) {
         try {
             console.log(data)
@@ -17,32 +21,47 @@ Router.post('/orderData', async (req, res) => {
             await Order.create({
                 email: req.body.email,
                 order_data: [data]
-            }).then(() => {
-                res.json({ success: true })
-            }).then(()=>{
-                Order.create({
-                    email: 'admin@gmail.com',
-                    order_data:[data]
+            }).then(async() => {
+                await Order.create({
+                    email:adminEmail,
+                    order_data: [data],
+                })
+                .then(()=>{
+                    return res.json({success:true});
+                })
+                .catch((err)=>{
+                    console.log("Error: ", err);
                 })
             })
         } catch (error) {
             console.log(error.message)
             res.send("Server Error", error.message)
-
         }
     }
-
     else {
         try {
             await Order.findOneAndUpdate({ email: req.body.email },
-                { $push: { order_data: data } }).then(() => {
-                    res.json({ success: true })
+                { $push: { order_data: data } }).then(async () => {
+                    await Order.findOneAndUpdate({
+                        email: adminEmail
+                    },
+                    {
+                        $push: {order_data: data}
+                    }
+                    )
+                    .then(()=>{
+                        return res.json({success:true});
+                    })
+                    .catch((err)=>{
+                        console.log("Error: ", err);
+                    })
                 })
         } catch (error) {
             console.log(error.message)
             res.send("Server Error", error.message)
         }
     }
+    
 })
 
 Router.post('/myOrders', async (req, res) => {
